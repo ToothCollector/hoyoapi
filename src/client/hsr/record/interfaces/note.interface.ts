b32f88d9aab5a11f6b304e12a0b3933ebd61bba4
Itamar <zamir.itamar@gmail.com>
2024-12-14 22:58:02 +0200

    feat(note.interface.ts): added more details to note

--- src/client/hsr/record/interfaces/note.interface.ts
+++ src/client/hsr/record/interfaces/note.interface.ts
@@ -3,11 +3,15 @@ export interface IHSRExpedition {
   status: string
   remaining_time: number
   name: string
+  item_url: string
 }
 export interface IHSRNote {
   current_stamina: number
   max_stamina: number
+  current_reserve_stamina: number
+  is_reserve_stamina_full: boolean
   stamina_recover_time: number
+  stamina_full_ts: number
   accepted_epedition_num: number
   total_expedition_num: number
   expeditions: IHSRExpedition[]
